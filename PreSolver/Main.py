a2ae3163d88778e82dab4aba9a786510b2bf4d8b
Ruth Bergin <ruthhelenbergin@gmail.com>
2023-01-10 22:32:27 +0000

    Modify Main.py to demonstrate current capacity.

--- PreSolver/Main.py
+++ PreSolver/Main.py
@@ -9,24 +9,33 @@ def ii_dataset():
     n = 41
 
     path = "instances/inductive-inference"
+    i = 1
     for fn in os.listdir(path):
-        print(f"Executing {fn}")
+        print(f"On file {i} ~ {fn}")
         start_time = time()
         file = open(path+"/"+fn, "r")
         cnf_string = file.read()
+        print("\n", cnf_string[cnf_string.find("p cnf"):cnf_string.find("p cnf")+17], "\n")
         file.close()
 
         vs = VariableSelector(cnf_string, cutoff=-0.5)
         vs.run()
 
-        times.append(time() - start_time)
+        time_taken = time() - start_time
+        times.append(time_taken)
+        print(f"Time taken:\t{time_taken//60} minutes {time_taken%60} seconds")
 
         if vs.solved:
             num_solved += 1
+            print("solved")
 
-    print("INDUCTIVE INFERENCE")
-    print("PROPORTION SOLVED:\t{}".format(num_solved / n))
-    print("MIN:\t{}\nMAX:\t{}\nMEAN:\t{}\n".format(min(times), max(times), mean(times)))
+        print("\n~~~~~~~\n")
+        i += 1
+
+
+    string = f"PROPORTION SOLVED:\t{num_solved / n}\nTIMING:\nMIN:\t{min(times)}\tMAX:\t{max(times)}\tMEAN:\t{mean(times)}\nTimes:\t{times}"
+
+    print(string)
 
 def CBS_dataset():
     times = []
