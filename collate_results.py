b3d896dbe40d4ba028473ad41561dc6b787962d7
Ruth Helen Bergin <119401946@umail.ucc.ie>
2023-08-11 10:12:29 +0100

    Additional scripts for generating and collating results.

---
+++ collate_results.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+def main(input_folder, output_file):
+    print(f"Writing collated results to output file {output_file}")
+    file = open(output_file, "w")
+    file.write("instance,tries,cpu_time\n")
+    file.close()
+    for filename in os.listdir(input_folder):
+        fn = open(input_folder+filename, "r")
+        messy = [line.strip("\n") for line in fn.readlines()]
+        fn.close()
+        tries_index, cpu_index = -1,-1
+        for index,line in enumerate(messy):
+            if "tries" in line:
+                tries_index = index
+            elif "CPU Time" in line:
+                cpu_index = index
+        tries = messy[tries_index].split(" ")[-1]
+        cpu_time =[word for word in messy[cpu_index].strip("\n").split(" ") if word!=""][-1]
+        out = open(output_file, "a")
+        out.write(f"{filename},{tries},{cpu_time}\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", required=True, type=str)
+    parser.add_argument("-e","--experiment", required=True, type=str)
+    parser.add_argument("-r", "--randomisationProb", required=True, type=int)
+
+    args = parser.parse_args()
+
+    input_folder = path = f"PreSolver/PreSolver/instances/{args.dataset}/results_{args.experiment}_{args.randomisationProb}/"
+    if "/" in args.dataset:
+        raise ValueError("Restructure folders so instances are not in subfolder.")
+    output_file = f"results/{args.dataset}_{args.experiment}_{args.randomisationProb}_results.txt"
+    main(
+        input_folder,
+        output_file
+    )
---
+++ execute_solver_on_instance_set.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+usage() {
+  echo "Usage: $0 <dataset name> [initialisation] [randomisationProb]"
+}
+
+handle_error() {
+  echo "Error: $1"
+  usage
+  exit 1
+}
+
+# trap 'handle_error "An error occurred."' ERR
+
+if [ $# -lt 1 ]; then
+  handle_error "At least one argument is required."
+fi
+
+dataset=$1
+
+if [ $# -lt 2 ]; then
+    initialisation="control"
+  else
+    initialisation=$2
+fi
+
+if [ $# -lt 3 ]; then
+    randomisationProb=25
+  else
+    randomisationProb=$3
+fi
+
+instance_folder="PreSolver/PreSolver/instances/${dataset}/"
+
+if [ $initialisation = "control" ]; then
+  output_folder="${instance_folder}results_control_0/"
+  randomisationProb=0
+else
+  output_folder="${instance_folder}results_${initialisation}_${randomisationProb}/"
+fi
+
+solver="PreSolver/PreSolver/probSAT-master/probSAT"
+
+if [ -d "$output_folder" ]; then
+  rm -r "$output_folder"
+fi
+mkdir $output_folder
+
+for file in "$instance_folder"*.cnf; do
+  filename=$(basename "$file" .cnf)
+  echo "On file ${filename}"
+  if [ $initialisation = "control" ]; then
+    "$solver" -a "$file" > "${output_folder}${filename}.txt"
+  else
+      initialisation_file="${instance_folder}/${initialisation}_predictions/${filename}.txt"
+      "$solver" -a -i "$initialisation_file" -r "$randomisationProb" "$file" > "${output_folder}${filename}.txt"
+  fi
+done
+
+python collate_results.py -d "$dataset" -e "$initialisation" -r "$randomisationProb"
